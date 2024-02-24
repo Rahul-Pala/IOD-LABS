79f568bc5a935a5ad17449d7813390e1853f8613
Rahul-Pala <rahul_pala@hotmail.com>
2024-02-24 16:18:42 +1300

    Module 1 Labwork

---
+++ Module 1/Exercise4.js
@@ -0,0 +1,34 @@
+function sayHello() {
+    console.log('Hello')
+   }
+
+   sayHello()
+
+   function sum(a, b)
+   {
+    console.log(a + b)
+   }
+sum(5,1)   
+
+function summ(c, d)
+{
+    console.log(c - d)
+
+    summ(4, 1)
+
+}
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
